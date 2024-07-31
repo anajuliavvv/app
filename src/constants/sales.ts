ae96bb23398072e1cdb6dc2fa9f9631c61cdb043
anajuliavvv <anajuliavvv@proton.me>
2024-07-31 18:10:18 -0300

    Add files via upload

--- src/constants/sales.ts
+++ src/constants/sales.ts
@@ -4,29 +4,173 @@ export interface ISale {
 }
 
 export const sales: Record<string, ISale> = {
+  ["0T09:31:15"]: {
+    name: "💳 Saldo",
+    value: 1400,
+  },
   ["0T10:48:32"]: {
-    name: "💳 Virtual",
-    value: 500,
+    name: "💳 Consultável",
+    value: 3000,
   },
   ["0T14:48:32"]: {
-    name: "💳 Virtual",
-    value: 850,
+    name: "💳 Físico",
+    value: 700,
+  },
+  ["0T18:31:15"]: {
+    name: "💳 Saldo",
+    value: 1400,
+  },
+  ["0T19:31:15"]: {
+    name: "💳 Saldo",
+    value: 700,
   },
   ["1T18:48:32"]: {
-    name: "💳 Virtual",
-    value: 850,
+    name: "💳 Consultável",
+    value: 5500,
+  },
+  ["1T15:10:12"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+  ["1T13:51:43"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+
+  ["2T13:51:33"]: {
+    name: "💳 Notas",
+    value: 900,
+  },
+
+  ["2T19:21:55"]: {
+    name: "💳 Saldo",
+    value: 700,
   },
 
   ["2T20:15:10"]: {
     name: "💳 Físico",
-    value: 500,
+    value: 1050,
   },
-  ["2T20:16:10"]: {
+  ["2T22:36:11"]: {
     name: "💳 Físico",
-    value: 1500,
+    value: 1050,
   },
+  ["2T23:16:05"]: {
+    name: "💳 Notas",
+    value: 900,
+  },
+
+  ["3T15:50:32"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+
+  ["3T20:41:05"]: {
+    name: "💳 Notas",
+    value: 1600,
+  },
+
+  ["3T12:35:03"]: {
+    name: "💳 Consultável",
+    value: 5500,
+  },
+
   ["3T19:50:32"]: {
     name: "💳 Físico",
-    value: 500,
+    value: 1600,
+  },
+  ["3T21:50:32"]: {
+    name: "💳 Notas",
+    value: 1600,
+  },
+  ["3T20:15:47"]: {
+    name: "💳 Notas",
+    value: 1600,
+  },
+
+  ////
+
+  ["4T09:31:15"]: {
+    name: "💳 Saldo",
+    value: 1400,
+  },
+  ["4T10:48:32"]: {
+    name: "💳 Consultável",
+    value: 3000,
+  },
+  ["4T14:48:32"]: {
+    name: "💳 Físico",
+    value: 700,
+  },
+  ["4T18:31:15"]: {
+    name: "💳 Saldo",
+    value: 1400,
+  },
+  ["4T19:31:15"]: {
+    name: "💳 Saldo",
+    value: 700,
+  },
+  ["5T18:48:32"]: {
+    name: "💳 Consultável",
+    value: 5500,
+  },
+  ["5T15:10:12"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+  ["5T13:51:43"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+
+  ["6T13:51:33"]: {
+    name: "💳 Notas",
+    value: 900,
+  },
+
+  ["6T19:21:55"]: {
+    name: "💳 Saldo",
+    value: 700,
+  },
+
+  ["6T20:15:10"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+  ["6T22:36:11"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+  ["6T23:16:05"]: {
+    name: "💳 Notas",
+    value: 900,
+  },
+
+  ["5T15:50:32"]: {
+    name: "💳 Físico",
+    value: 1050,
+  },
+
+  ["5T20:41:05"]: {
+    name: "💳 Notas",
+    value: 1600,
+  },
+
+  ["5T12:35:03"]: {
+    name: "💳 Consultável",
+    value: 5500,
+  },
+
+  ["5T19:50:32"]: {
+    name: "💳 Físico",
+    value: 1600,
+  },
+  ["5T21:50:32"]: {
+    name: "💳 Notas",
+    value: 1600,
+  },
+  ["5T20:15:47"]: {
+    name: "💳 Notas",
+    value: 1600,
   },
 };
